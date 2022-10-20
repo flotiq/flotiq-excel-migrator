596a29feb9bdc5dbea722f046570d148b5c94dff
WHLukasz <lukasz.wach@codewave.com>
2022-10-20 18:20:40 +0200

    co export working

--- converter.js
+++ converter.js
@@ -1,9 +1,12 @@
+const maxStringLength = 30000; //max string length allowed before throwing err (strings that are too long in single cell cause errors in ms excel)
+const referenceSep = ","; //used to seperate dataUrl for multiple references
+
 const ctdToHeader = (data) => {
     let row = [];
     for (field in data.schemaDefinition.allOf[1].properties) {
         let obj = {
             value: field,
-            fontWeight: `bold` // additionall cell properties here
+            fontWeight: `bold` // additionall cell properties for table headers here
         }
         row.push(obj);
     }
@@ -15,74 +18,150 @@ const ctdFieldTypes = (data) => {
 
     objTypes = {
         "richtext": String,
-        "textMarkdown": String,
+        "textMarkdown": "json",
         "text": String,
         "number": Number,
         "dateTime": Date,
-        "geo": "", //TBU
-        "datasource": "", //TBU
+        "geo": "json",
+        "datasource": "reference",
         "checkbox": Boolean,
+        "email": String,
+        "radio": String,
+        "select": String,
+        "object": "json",
+        "block": "json"
     }
 
     for (field in data.metaDefinition.propertiesConfig) {
-        fieldTypes[field] = objTypes[data.metaDefinition.propertiesConfig[field].inputType];
+        // console.log(data.metaDefinition.propertiesConfig[field].inputType); //DEL
+        fieldTypes[field] = {
+            propertyLabel: data.metaDefinition.propertiesConfig[field].label,
+            field: objTypes[data.metaDefinition.propertiesConfig[field].inputType]
+        }
     }
-    
     return fieldTypes;
 }
 
 const coToRecord = (data, fieldTypes) => {
     let row = [];
+    let coErrors = [];
+    // console.log(data);
 
     for (type in fieldTypes) {
-        let obj = formatContent(data[type], fieldTypes[type]);
-            
-        row.push(obj)
+        // console.log(data[type], fieldTypes[type].field)
+        let obj = formatContent(data[type], fieldTypes[type].field);
+        if (obj.error) {
+            coErrors.push({
+                propertyLabel: fieldTypes[type].propertyLabel,
+                message: obj.error
+            });
+        }
+        row.push(obj.element)
     }
-    return row;
+    return {
+        row: row,
+        coErrors: coErrors
+    }
 }
 
-// todo convert all flotiq data types
 const formatContent = (data, type) => {
+    let element;
+    let error = null;
+    if (!data || data.length === 0) {
+        return {
+            element: { value: null },
+            error: null
+        }
+    }
     switch (type) {
         case String:
-            return {
-                value: data,
-                type: type
+            error = validate(data, String);
+            element = {
+                value: data.substring(0, maxStringLength),
+                type: String
             }
+            break;
         case Number:
-            return {
+            element = {
                 value: data,
                 type: type
             }
+            break;
         case Date:
-            return {
-                format: "d mmmm yyyy",
-                value: dateIsValid(new Date(data)),
-                type: type
+            let date = new Date(data);
+            error = validate(date, type);
+            if (!error) {
+                element = {
+                    format: "d mmmm yyyy",
+                    value: date,
+                    type: type
+                }
+            } else {
+                element = {
+                    value: null
+                }
             }
+            break;
         case Boolean:
-            return {
+            element = {
                 value: data,
                 type: type
             }
+            break;
+        case "json":
+            data = JSON.stringify(data, null, 2);
+            error = validate(data, String);
+            element = {
+                value: data.substring(0, maxStringLength),
+                type: String
+            }
+            break;
+        case "reference":
+            let value = [];
+            for (let obj in data) {
+                value.push(data[obj].dataUrl);
+            }
+            element = {
+                value: data = value.join(referenceSep),
+                type: String
+            }
+            break;
         default:
-            return {
+            element = {
                 value: null
             }
+            break;
+    }
+    return {
+        element: element,
+        error: error
     }
 }
 
-function dateIsValid(date) {
-    if (
-        typeof date === 'object' &&
-        date !== null &&
-        typeof date.getTime === 'function' &&
-        !isNaN(date)
-    ) {
-        return date;
+let validate = (data, type) => {
+    let errorMessage = "";
+    switch (type) {
+        case Date:
+            if (
+                typeof data !== 'object' ||
+                data !== null ||
+                typeof data.getTime !== 'function' ||
+                isNaN(data)
+            ) {
+                errorMessage = `Invalid date`;
+            }
+            break;
+        case String:
+            if (data && data.length > maxStringLength) {
+                errorMessage = "Json object too long";
+                data = data.substring(0, 100) + "[...]";
+            }
+            break;
+        default:
+            errorMessage = "Unknown field type";
     }
-    return null;
+    if (errorMessage) {
+        return `${errorMessage}\nData: ${data}`;
+    } return null;
 }
-
 module.exports = { ctdToHeader, ctdFieldTypes, coToRecord };
--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const writeXlsxFile = require(`write-excel-file/node`);
 const fetch = require("node-fetch");
 const { ctdToHeader, ctdFieldTypes, coToRecord } = require('./converter');
 
-migrateXlsx = async (apiKey, ctdName, exportCo) => {
+migrateXlsx = async (apiKey, ctdName, limit = -1) => {
     let data = [];
+
     let ctd = await fetchContentTypeDefinition(apiKey, ctdName);
     
     if (ctd?.status < 200 || ctd?.status >= 300) {
-        console.log("Fetching content type failed:\n   Error", ctd.status, ":", ctd.statusText);
+        console.log(`Fetching content type failed:\n   Error ${ctd.status} : ${ctd.statusText}`);
         return;
     }
     
@@ -15,28 +16,44 @@ migrateXlsx = async (apiKey, ctdName, exportCo) => {
     
     data[0] = ctdToHeader(ctd);
     
-    if (exportCo === "--export-co") {
+    if (limit !== 0) {
         let co = await fetchContentObjects(apiKey, ctdName);
 
         if (co?.status < 200 || co?.status >= 300) {
-            console.log("Fetching content objects failed:\n   Error", co.status, ":", co.statusText);
+            console.log(`Fetching content objects failed:\n   Error ${co.status} : ${co.statusText}`);
             return;
         }
         co = await co.json();
 
         let fieldTypes = ctdFieldTypes(ctd);
 
+        let errors = [];
         let totalPages = co.total_pages;
+        let coExported = 0;
         let page = 1;
         while (page <= totalPages) {
-            for (let i = 0; i < co.count; i++) {
-                data.push(coToRecord(co.data[i], fieldTypes));
+            for (let i = 0; i < co.count && (limit === -1 || coExported < limit); i++) {
+                let result = (coToRecord(co.data[i], fieldTypes));
+                data.push(result.row);
+                if (result.coErrors.length !== 0) {
+                    errors.push(result.coErrors);
+                }
+                coExported++;
             }
             page++;
-            if (page <= totalPages) {
+            if (page <= totalPages && coExported <= limit) {
                 co = await fetchContentObjects(apiKey, ctdName, page);
             }
-        }   
+        }
+        if (errors.length !== 0) {
+            console.log(`Export errors occured!\n`)
+            for (let row in errors) {
+                console.log(`Errors in row ${Number(row)+1}:`);
+                for (let error in errors[row]) {
+                    console.log(`Error in property ${errors[row][error].propertyLabel}: ${errors[row][error].message}\n`)
+                }
+            }
+        }
     }
 
     await writeXlsxFile(data, {
@@ -60,5 +77,7 @@ const fetchContentObjects = async (apiKey, ctdName, page = 1, limit = 100) => {
 
 module.exports = { migrateXlsx };
 
+migrateXlsx("76ae0cce0fd42f1cbf8ef36b126e6717", "allFields", -1);
+
 // testing
-// migrateXlsx("[apiKey]", "[ctd name]", "--export-co" /*Optional, exports content objects*/);
+// migrateXlsx("[apiKey]", "[ctd name]", [limit], /*limit - how many co to export -1 = all co*/);
